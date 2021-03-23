87648cd917a8d98761a0ef26d51c8a457a65565b
Fumitaka.Kotou <fumikato294@gmail.com>
2021-03-23 16:24:28 +0900

    進む/戻るボタンを押した際に配列順に画像を送り、且つループするようなコードに書き換えた。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var RevButton: UIButton!
     
     // 配列に指定するindex番号を宣言
+
     var nowIndex:Int = 0
 
     // スライドショーに使用するタイマーを宣言
@@ -22,27 +23,13 @@ class ViewController: UIViewController {
 
     // スライドショーさせる画像の配列を宣言
     var imageArray:[UIImage] = [
+        UIImage(named: "No.3.jpg")!,
         UIImage(named: "No.1.jpg")!,
-        UIImage(named: "No.2.jpg")!,
-        UIImage(named: "No.3.jpg")!
+        UIImage(named: "No.2.jpg")!
       ]
     
     @IBOutlet weak var imageView: UIImageView!
     
-    var image1: UIImage!
-    var image2: UIImage!
-    var image3: UIImage!
-    
-    var adv = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        image1 = UIImage(named:"img1")
-        image2 = UIImage(named:"img2")
-        image3 = UIImage(named:"img3")
-        
-    }
     
     // 再生ボタンを押した時の処理
     @IBAction func slideShowButton(_ sender: Any) {
@@ -92,36 +79,32 @@ class ViewController: UIViewController {
     
     // 進むボタン
     @IBAction func advance(_ sender: Any) {
-        adv += 1
+        nowIndex += 1
     
-        switch adv {
-        case 1:
-            imageView.image = image1
-        case 2:
-            imageView.image = image2
-        case 3:
-            imageView.image = image3
-        default:
-            imageView.image = image1
+        // indexが表示予定の画像の数と同じ場合
+        if (nowIndex == imageArray.count) {
+            // indexを一番最初の数字に戻す
+            nowIndex = 0
         }
+        // indexの画像をstoryboardの画像にセットする
+        imageView.image = imageArray[nowIndex]
     }
     
     // 戻るボタン
     @IBAction func reverse(_ sender: Any) {
-        adv -= 1
+        nowIndex -= 1
         
-        switch adv {
-        case 3:
-            imageView.image = image3
-        case 2:
-            imageView.image = image2
-        case 1:
-            imageView.image = image1
-        default:
-            imageView.image = image3
+        if (nowIndex < 0) {
+            nowIndex = 2
         }
+        // indexが表示予定の画像の数と同じ場合
+        if (nowIndex == imageArray.count) {
+            // indexを一番最初の数字に戻す
+            nowIndex = 2
+        }
+        // indexの画像をstoryboardの画像にセットする
+        imageView.image = imageArray[nowIndex]
     }
-    
 
 }
 
